7279bac10b6a7f2c554d40584652f119d71f81a9
Nile Okomo <okomonile3@gmail.com>
2021-12-04 14:38:30 +0300

    Open book

---
+++ bookreader.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import sys
+from gtts import gTTS
+
+language = 'en'
+book = sys.argv[1]
+
+
+def read_book(book):
+    with open(book, 'r') as f:
+        text = f.read()
+        return text
+
+
+def speak(text):
+    tts = gTTS(text=text, lang=language, slow=False)
+    tts.save('book.mp3')
+    # os.system('mpg321 book.mp3')
+
+
+if __name__ == '__main__':
+    speak(read_book(book))
